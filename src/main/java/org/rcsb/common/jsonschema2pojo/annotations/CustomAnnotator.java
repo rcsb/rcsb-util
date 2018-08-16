9fd36901325536b26b92f9469c8ac21caf2ec1bc
valasatava <yana.valasatava@gmail.com>
2018-08-16 16:22:44 -0700

    custom annotator implementation

---
+++ src/main/java/org/rcsb/common/jsonschema2pojo/annotations/CustomAnnotator.java
@@ -0,0 +1,46 @@
+package org.rcsb.common.jsonschema2pojo.annotations;
+
+import com.fasterxml.jackson.annotation.JsonPropertyDescription;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.sun.codemodel.JDefinedClass;
+import com.sun.codemodel.JFieldVar;
+import com.sun.codemodel.JMethod;
+import org.jsonschema2pojo.AbstractAnnotator;
+import org.jsonschema2pojo.GenerationConfig;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Annotates generated Java types using the custom annotations.
+ *
+ * @author Yana Valasatava
+ * @since 1.4.0
+ */
+public class CustomAnnotator extends AbstractAnnotator {
+
+    private Map<String, String> description = new HashMap<>();
+
+    public CustomAnnotator(GenerationConfig generationConfig) {
+        super(generationConfig);
+    }
+
+    public void propertyField(JFieldVar field, JDefinedClass clazz, String propertyName, JsonNode propertyNode) {
+
+        if (propertyNode.has("description")) {
+            description.put(propertyName, propertyNode.get("description").asText());
+        }
+    }
+
+    @Override
+    public void propertyGetter(JMethod getter, String propertyName) {
+        if (description.containsKey(propertyName))
+            getter.annotate(JsonPropertyDescription.class).param("value", description.get(propertyName));
+    }
+
+    @Override
+    public void propertySetter(JMethod setter, String propertyName) {
+        if (description.containsKey(propertyName))
+            setter.annotate(JsonPropertyDescription.class).param("value", description.get(propertyName));
+    }
+}
